dcf355e32f01ac8f6bfeb75b4068fb931c8e7ded
DAIZHENWEI <32122197+DAIZHENWEI@users.noreply.github.com>
2019-06-02 21:07:38 +0800

    Add files via upload

---
+++ BDMMA_MacOS/R/Microbiome_dat.R
@@ -0,0 +1,10 @@
+#' Taxonomy Reads and Associated Phenotypes
+#'
+#' Simualated taxonomy read counts of 40 taxa and their associated
+#' phenotypes.
+#'
+#' @format SummarizedExperiment
+#' @details The dataset contains the simualated taxonomy read counts from 80 samples,
+#' where the samples come from 4 different batches and include both case and control
+#' samples in each batch. For the detailed usuage, please see the package vignette.
+"Microbiome_dat"
